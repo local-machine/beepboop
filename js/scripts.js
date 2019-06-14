234f6444ee2e2de34bc96e94ba448579cf6f4fd9
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-06-14 14:00:25 -0700

    Adding countTo method

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //Business Logic
 function halize(userNum) {
   debugger;
+  countTo
   var search = {'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}, roman = '', i;
   for (i in search) {
     while (userNum >= search[i] ) {
