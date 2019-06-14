d46b8f5fb19d3ee937ef5800c379c62c3133566e
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-06-14 16:57:21 -0700

    made small changes but incomplete

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ var
 function halize(userNum) {
   debugger;
 
-  var search = {'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}, roman = '', i;
+  var search = [], roman = '', i;
   for (i in search) {
     while (userNum >= search[i] ) {
-    roman += i;
+    hal += i;
     userNum -= search[i];
   }
 }
-return roman;
+return hal;
 }
 
 
