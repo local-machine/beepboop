2d320743b03af5adf05cb2601081d939b3674192
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-06-18 06:44:16 -0700

    Added nested if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,43 @@
 //Business Logic
-var
-function halize(userNum) {
-  debugger;
 
-  var search = [], roman = '', i;
-  for (i in search) {
-    while (userNum >= search[i] ) {
-    hal += i;
-    userNum -= search[i];
+// function halize(userNum) {
+//   var numArray = [];
+//   for (var i = 0; i <= userNum; i++) {
+//     numArray.push(i)
+//     var newString = numArray.toString()
+//     var newString = newString.split("");
+//     console.log(newString);
+//     if (newString.includes("1")) {
+//       var outputStr = newString.replace("1", "Beep!");
+//       console.log(outputStr);
+//     } else {
+//
+//     }
+//   }
+// }
+
+var sorry = "I'm sorry, Dave. I'm afraid I can't do that.";
+
+var halize = num => {
+  num = num.toString();
+  if (num.includes('1')) {
+    if (num.length > 1) {
+      return sorry;
+    } else {
+      return 'Beep!';
+    }
+  } else if (num.includes('2')){
+    if (num.length > 1) {
+      return sorry;
+    } else {
+      return 'Boop!';
+    }
+  } else if (num.includes('3')) {
+    return sorry;
+  } else {
+    return num;
   }
-}
-return hal;
-}
-
-
-
+};
 
 //UI
 $(document).ready(function() {
